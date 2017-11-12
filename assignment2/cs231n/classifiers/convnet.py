4b7e7f97a5f651a794383f7b49beaac3492e1d55
Lucas <scofieldgood@gmail.com>
2017-11-12 12:02:19 +0800

    finished assignment2; added assignment 3

--- assignment2/cs231n/classifiers/convnet.py
+++ assignment2/cs231n/classifiers/convnet.py
@@ -6,11 +6,43 @@ from cs231n.layer_utils import *
 
 from cs231n.classifiers.fc_net import *
 
+
+def conv_bn_relu_pool_forward(x, w, b, gamma, beta, conv_param, pool_param, bn_param):
+  """
+  Convenience layer that performs a convolution, a ReLU, and a pool.
+
+  Inputs:
+  - x: Input to the convolutional layer
+  - w, b, conv_param: Weights and parameters for the convolutional layer
+  - pool_param: Parameters for the pooling layer
+
+  Returns a tuple of:
+  - out: Output from the pooling layer
+  - cache: Object to give to the backward pass
+  """
+  a, conv_cache = conv_forward_fast(x, w, b, conv_param)
+  a, bn_cache = spatial_batchnorm_forward(a, gamma, beta, bn_param)
+  s, relu_cache = relu_forward(a)
+  out, pool_cache = max_pool_forward_fast(s, pool_param)
+  cache = (conv_cache, bn_cache, relu_cache, pool_cache)
+  return out, cache
+
+def conv_bn_relu_pool_backward(dout, cache):
+  """
+  Backward pass for the conv-relu-pool convenience layer
+  """
+  conv_cache, bn_cache, relu_cache, pool_cache = cache
+  ds = max_pool_backward_fast(dout, pool_cache)
+  da = relu_backward(ds, relu_cache)
+  da, dgamma, dbeta = spatial_batchnorm_backward(da, bn_cache)
+  dx, dw, db = conv_backward_fast(da, conv_cache)
+  return dx, dw, db, dgamma, dbeta
+
 class MyConvNet(object):
   """
   A three-layer convolutional network with the following architecture:
   
-  conv - relu - 2x2 max pool - FullyConnectedNet
+  [conv - batch norm - relu - 2x2 max pool] * N - FullyConnectedNet
   
   The network operates on minibatches of data that have shape (N, C, H, W)
   consisting of N images, each with height H and width W and with C input
@@ -18,7 +50,7 @@ class MyConvNet(object):
   """
   
   def __init__(self, input_dim, fc_hidden_dims, num_classes, 
-               num_filters=32, filter_size=7, weight_scale=1e-3, reg=0.0,
+               num_filters=[32], filter_size=7, weight_scale=1e-3, reg=0.0,
                dropout=0., use_batchnorm=False,
                dtype=np.float32, seed=None):
     """
@@ -38,8 +70,8 @@ class MyConvNet(object):
     self.params = {}
     self.reg = reg
     self.dtype = dtype
+    self.num_conv_layers = len(num_filters)
     
-    # My Changes
     self.F, self.WW = num_filters, filter_size
     self.C, self.H, self.W = input_dim
     
@@ -49,14 +81,26 @@ class MyConvNet(object):
     # pass pool_param to the forward pass for the max-pooling layer
     self.pool_param = {'pool_height': 2, 'pool_width': 2, 'stride': 2}
     
-    self.params['W1'] = weight_scale * np.random.randn(self.F, self.C, self.WW, self.WW)
-    self.params['b1'] = weight_scale * np.zeros(self.F)
-    # Input = (F, H/2, W/2), will be flatened in affine layer, Output = (hidden_dim,)
-    self.fc = FullyConnectedNet(fc_hidden_dims, self.F * self.H/2 * self.W/2, num_classes=num_classes, 
+    
+    for l in xrange(self.num_conv_layers):
+      input_channel_num = self.F[l - 1] if l > 0 else self.C
+      w_, b_ = w_b(l)
+      self.params[w_] = weight_scale * np.random.randn(self.F[l], input_channel_num, self.WW, self.WW)
+      self.params[b_] = weight_scale * np.zeros(self.F[l])
+        
+      gamma_, beta_ = get_gamma_beta_key(l)
+      self.params[gamma_] = np.ones(self.F[l])
+      self.params[beta_] = np.zeros(self.F[l])
+        
+    # Input = (F, H/2, W/2), will be flatened in affine layer
+    input_d = self.F[-1] * self.H/(2**self.num_conv_layers) * self.W/(2**self.num_conv_layers)
+    self.fc = FullyConnectedNet(fc_hidden_dims, input_d, num_classes=num_classes, 
                                 dropout=dropout, use_batchnorm=use_batchnorm, reg=reg, weight_scale=weight_scale, 
                                 dtype=dtype, seed=seed)
     for p, val in self.fc.params.items():
       self.params['fc:'+p] = val
+    
+    self.bn_params = [{'mode': 'train'} for i in xrange(self.num_conv_layers)]
 
     for k, v in self.params.iteritems():
       self.params[k] = v.astype(dtype)
@@ -68,24 +112,44 @@ class MyConvNet(object):
     
     Input / output: Same API as TwoLayerNet in fc_net.py.
     """
-    W1, b1 = self.params['W1'], self.params['b1']
+    X = X.astype(self.dtype)
+    mode = 'test' if y is None else 'train'
+ 
+    for bn_param in self.bn_params:
+      bn_param[mode] = mode
+    
     for p in self.fc.params.keys():
       self.fc.params[p] = self.params['fc:'+p]
 
-
-    X, cache1 = conv_relu_pool_forward(X, W1, b1, self.conv_param, self.pool_param)
+    ############################################################################
+    #                             FORWARD PASS                                 #
+    ############################################################################     
+    caches = []
+    for l in xrange(self.num_conv_layers):
+      w_, b_ = w_b(l)
+      gamma_, beta_ = get_gamma_beta_key(l)
+      X, cache_ = conv_bn_relu_pool_forward(X, self.params[w_], self.params[b_], self.params[gamma_], self.params[beta_], 
+                                            self.conv_param, self.pool_param, self.bn_params[l])
+      caches.append(cache_)
     
     if y is None:
       scores = self.fc.loss(X)
       return scores
     
+    ############################################################################
+    #                             BACK PROPAGATION                            #
+    ############################################################################    
     grads = {}
     loss, fc_grads = self.fc.loss(X, y)
-    _,  grads['W1'], grads['b1'] = conv_relu_pool_backward(fc_grads['x'], cache1)    
     
-    for w in ['W1']:
-      loss += 0.5 * self.reg * np.sum(self.params[w]**2)
-      grads[w] += self.reg * self.params[w]
+    dx = fc_grads['x']
+    for l in reversed(range(self.num_conv_layers)):
+      w_, b_ = w_b(l)
+      gamma_, beta_ = get_gamma_beta_key(l)
+      dx,  grads[w_], grads[b_], grads[gamma_], grads[beta_] = conv_bn_relu_pool_backward(dx, caches[l])    
+    
+      loss += 0.5 * self.reg * np.sum(self.params[w_]**2)
+      grads[w_] += self.reg * self.params[w_]
         
     for p in self.fc.params.keys():
       grads['fc:'+p] = fc_grads[p]
